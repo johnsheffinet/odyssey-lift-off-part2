26adc8aa14aa856a85d82921ffdb4348460ca747
John William Sheffield Jr <john@sheffinet.org>
2022-12-03 12:06:47 -0500

    Create resolvers.js

---
+++ server/src/resolvers.js
@@ -0,0 +1,8 @@
+export const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate
+    // the homepage grid of our web client
+    tracksForHome: (parent, args, context, info) => {},
+  }
+};
+
