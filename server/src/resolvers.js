e87e1acb9f4464bdcdcbe7ac8293ce4788564d65
John William Sheffield Jr <john@sheffinet.org>
2022-12-03 12:17:00 -0500

    Update resolvers.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,8 +1,9 @@
 export const resolvers = {
   Query: {
-    // returns an array of Tracks that will be used to populate
-    // the homepage grid of our web client
-    tracksForHome: (parent, args, context, info) => {},
+    // get all tracks, will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, {dataSources}) => {
+      return dataSources.trackAPI.getTracksForHome();
+    }
   }
 };
 
